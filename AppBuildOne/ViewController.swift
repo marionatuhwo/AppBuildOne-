5d0f207b9055360dd804d71215ecbc5ad42f35da
Marion <ano@hawaii.edu>
2020-02-12 16:16:14 -1000

    Initial Commit

---
+++ AppBuildOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppBuildOne
+//
+//  Created by Marion Ano on 2/12/20.
+//  Copyright © 2020 Marion Ano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
